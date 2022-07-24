d3f686922a3d71e2fada2e8b6f6c735abce23ce5
Nelson <104731311+nelson9608@users.noreply.github.com>
2022-07-24 03:10:20 -0400

    Update heroku.sh

--- heroku.sh
+++ heroku.sh
@@ -10,26 +10,6 @@ python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/delay" $DELAY
 python3 -m simplebot -a "$ADDR" db -s "simplebot_translator/filter_enabled" "no"
 python3 ./restore_keys.py
 
-# add the web_comress plugin
-python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_compress.py'); open('web_compress.py', 'wb').write(r.content)"
-python3 -m simplebot -a "$ADDR" plugin --add ./web_compress.py
-
-# add the web_search plugin
-python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_search.py'); open('web_search.py', 'wb').write(r.content)"
-python3 -m simplebot -a "$ADDR" plugin --add ./web_search.py
-
-# add the youtube plugin
-python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/youtube.py'); open('youtube.py', 'wb').write(r.content)"
-python3 -m simplebot -a "$ADDR" plugin --add ./youtube.py
-
-# add the misc plugin
-python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/misc.py'); open('misc.py', 'wb').write(r.content)"
-python3 -m simplebot -a "$ADDR" plugin --add ./misc.py
-
-# add the image_search plugin
-python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/image_search.py'); open('image_search.py', 'wb').write(r.content)"
-python3 -m simplebot -a "$ADDR" plugin --add ./image_search.py
-
 # add the encryption_error plugin to leverage key changes
 python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/encryption_error.py'); open('encryption_error.py', 'wb').write(r.content)"
 python3 -m simplebot -a "$ADDR" plugin --add ./encryption_error.py
